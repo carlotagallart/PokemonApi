7ec657e9342d750e7c19416081f53d5466898cb9
Carlota <carlota.gallart@gmail.com>
2023-10-17 13:50:29 +0200

    consumint la api i mostrant en pantalla

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import "./App.css";
 import CardColumn from "./components/cardColumn";
 import CardList from "./components/cardList";
@@ -7,6 +8,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGrip, faList } from "@fortawesome/free-solid-svg-icons";
 function App() {
   const [active, setActive] = useState(true);
+  const [loading, setLoading] = useState(false)
+  const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [urlApi, setUrlApi] = useState('https://pokeapi.co/api/v2/pokemon/')
+
+  useEffect(() => {
+    setLoading(true);
+    const getApi = async () => {
+      // consumim la API
+      const res = await axios.get(urlApi);
+      //agafem la data que hi ha dins
+      const data = res.data.results;
+      // fem un map per poder agafar les URLs de la info de cada Pokemon
+      const dataInfo = data.map(async (pokemon) => {
+        // consumim la url de cada pokemon
+        const urlPokemonInfo = await axios.get(pokemon.url);
+        // agafem i retornem la data
+        const pokemons = await urlPokemonInfo.data;
+        return pokemons;
+      });
+      // executem l'array de promeses
+      setPokemonInfo(await Promise.all(dataInfo));
+      setLoading(false);
+    };
+    // executem getAPI
+    getApi()
+
+  }, [urlApi]);
+
+
+  
   return (
     <div className="App">
       <button onClick={() => setActive(!active)} disabled={active}>
@@ -16,7 +47,7 @@ function App() {
         <FontAwesomeIcon icon={faList} />
       </button>
       
-      {(active && <CardColumn />) || (!active && <CardList />)}
+      {(active && <CardColumn pokemonInfo={pokemonInfo} />) || (!active && <CardList />)}
     </div>
   );
 }
--- src/components/cardColumn.js
+++ src/components/cardColumn.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-export default function CardColumn() {
+export default function CardColumn({pokemonInfo}) {
   return(
     <div>
-        VISTA COLUMNA
+        {
+            pokemonInfo.map(pokemon =>{
+               return (
+               <div key={pokemon.id}>
+                <p>{pokemon.name}</p>
+                <img src={pokemon.sprites.other.dream_world.front_default} />
+               </div>
+
+               )
+            })
+        }
     </div>
   )
 }
