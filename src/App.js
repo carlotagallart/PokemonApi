e2744064741f0dd546a6b2f8ee2cd0f1bb85dd8b
Carlota <carlota.gallart@gmail.com>
2023-10-17 14:23:32 +0200

    paginacio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+// importem axios
 import axios from 'axios';
 import "./App.css";
+// importem els components
 import CardColumn from "./components/cardColumn";
 import CardList from "./components/cardList";
 // npm + importem fontawesome + icones 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGrip, faList } from "@fortawesome/free-solid-svg-icons";
+import { faGrip, faList, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 function App() {
   const [active, setActive] = useState(true);
   const [loading, setLoading] = useState(false)
   const [pokemonInfo, setPokemonInfo] = useState([]);
-  const [urlApi, setUrlApi] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const [urlApi, setUrlApi] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const [nextPage, setNextPage] = useState();
+  const [prevPage, setPrevPage] = useState();
 
   useEffect(() => {
     setLoading(true);
@@ -19,6 +23,8 @@ function App() {
       const res = await axios.get(urlApi);
       //agafem la data que hi ha dins
       const data = res.data.results;
+      setNextPage(res.data.next);
+      setPrevPage(res.data.previous);
       // fem un map per poder agafar les URLs de la info de cada Pokemon
       const dataInfo = data.map(async (pokemon) => {
         // consumim la url de cada pokemon
@@ -48,6 +54,15 @@ function App() {
       </button>
       
       {(active && <CardColumn pokemonInfo={pokemonInfo} />) || (!active && <CardList />)}
+      {/* mirem que tingui url de prev o next si te la mostrem si no te no ho mostem */}
+      {prevPage && <button onClick={()=>{
+        setUrlApi(prevPage)
+      }
+      }> <FontAwesomeIcon icon={faArrowLeft} /></button>}
+      {nextPage && <button onClick={()=>{
+        setUrlApi(nextPage)
+      }
+      }> <FontAwesomeIcon icon={faArrowRight} /></button>}
     </div>
   );
 }
